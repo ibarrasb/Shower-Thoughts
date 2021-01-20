758cd944f21bcd7291a227a9f4f902788c2c7689
kingdonut23 <68092804+kingdonut23@users.noreply.github.com>
2021-01-19 22:03:19 -0600

    Updated schema
    SQL had a redundant table, needed Customer removed. Users is the new table

--- schema.sql
+++ schema.sql
@@ -4,13 +4,6 @@ CREATE DATABASE showerthoughts_db;
 
 USE showerthoughts_db;
 
-CREATE TABLE customer (
-	cust_id INT AUTO_INCREMENT NOT NULL,
-	cust_nme VARCHAR(30) NOT NULL,
-    cust_email VARCHAR(255) NOT NULL,
-    psword INT NOT NULL,
-    PRIMARY KEY(cust_id)
-);
 
 CREATE TABLE category (
 	cat_id int AUTO_INCREMENT NOT NULL,
@@ -27,7 +20,7 @@ CREATE TABLE posts (
     cust_id int,
     PRIMARY KEY (post_id),
     FOREIGN KEY (cat_id) REFERENCES category(cat_id),
-    FOREIGN KEY (cust_id) REFERENCES customer(cust_id)
+    FOREIGN KEY (cust_id) REFERENCES users(id)
 );
 
 CREATE TABLE reply (
@@ -36,6 +29,6 @@ CREATE TABLE reply (
     cust_id int,
     post_id int,
     PRIMARY KEY (rep_id),
-	FOREIGN KEY (cust_id) REFERENCES customer(cust_id),
+	FOREIGN KEY (cust_id) REFERENCES users(id),
 	FOREIGN KEY (post_id) REFERENCES posts (post_id)
 );
