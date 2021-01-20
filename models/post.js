a6e24b2c5ff0646ab8e6123dfc8ca256f5978983
kingdonut23 <68092804+kingdonut23@users.noreply.github.com>
2021-01-19 22:01:48 -0600

    Updated models

---
+++ models/post.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+    var Post = sequelize.define("Posts", {
+      post_title: DataTypes.STRING,
+      post_desc: DataTypes.STRING,
+      post_date: DataTypes.DATE
+    });
+  
+    Post.associate = function(models) {
+      Post.belongsTo(models.Category, {
+        foreignKey: {
+          name: 'cat_id', 
+          targetKey: 'id',
+          allowNull: false
+        }
+});
+    };
+    Post.associate = function(models) {
+      Post.belongsTo(models.User, {          
+        foreignKey: {
+        name: 'user_id', 
+        targetKey: 'id',
+        allowNull: false
+      }
+});
+  
+  };
+  return Post;
+};
